8ab4796b3a7d775542ec04c116553edbea7033ee
Henare Degan <henare.degan@gmail.com>
2013-11-25 12:46:09 -0200

    Save to CSV instead of a ScraperWiki SQLite database

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'scraperwiki'
 require 'mechanize'
+require 'csv'
 
 agent = Mechanize.new
 base_url = 'http://lobbyists.pmc.gov.au/'
@@ -15,59 +15,53 @@ agencies = table.at(:tbody).search(:tr).map do |row|
    details_last_updated: Date.parse(row.search(:td)[4].inner_text)}
 end
 
+agencies_csv = CSV.new(File.open('agencies.csv', 'w'))
+agencies_csv << ['id', 'url', 'business_entity_name', 'trading_name', 'abn', 'details_last_updated']
+
+owners_csv = CSV.new(File.open('owners.csv', 'w'))
+owners_csv << ['agency_id', 'agency_business_entity_name', 'owner_name']
+
+lobbyists_csv = CSV.new(File.open('lobbyists.csv', 'w'))
+lobbyists_csv << ['agency_id', 'agency_business_entity_name', 'lobbyist_name', 'position', 'former_government_representative', 'cessation_date']
+
+clients_csv = CSV.new(File.open('clients.csv', 'w'))
+clients_csv << ['agency_id', 'agency_business_entity_name', 'client_name']
+
 agencies.each do |agency|
   agency_id = agency[:url].match('id=(.*)')[1]
 
-  if !ScraperWiki.select("* FROM agencies WHERE `abn`='#{agency[:abn]}' AND `details_last_updated`='#{agency[:details_last_updated]}'").empty? 
-    puts "Skipping existing record agency ID #{agency_id}"
-    next
-  end
-
   agency_page = agent.get agency[:url]
-  
-  agency_details = {
-    id: agency_id,
-    url: agency[:url],
-    business_entity_name: agency[:business_entity_name],
-    trading_name: agency[:trading_name],
-    abn: agency[:abn],
-    details_last_updated: agency[:details_last_updated],
-    date_scraped: Date.today
-  }
 
-  ScraperWiki.save_sqlite([:id], agency_details, 'agencies')
+  agencies_csv << [
+    agency_id,
+    agency[:url],
+    agency[:business_entity_name],
+    agency[:trading_name],
+    agency[:abn],
+    agency[:details_last_updated],
+  ]
 
   # Save owners
-  owners = agency_page.at('#profile').search(:ul).last.search(:li).map do |li|
-    {agency_id: agency_id,
-     agency_business_entity_name: agency[:business_entity_name],
-     owner_name: li.inner_text}
+  owners_csv << agency_page.at('#profile').search(:ul).last.search(:li).map do |li|
+     [agency_id, agency[:business_entity_name], li.inner_text]
   end
-
-  ScraperWiki.save_sqlite([:agency_id, :owner_name], owners, 'owners')
 
   # Save lobbyists
   if agency_page.at('#lobbyistDetails')
-    lobbyists = agency_page.at('#lobbyistDetails').at(:tbody).search(:tr).map do |row|
-      {agency_id: agency_id,
-       agency_business_entity_name: agency[:business_entity_name],
-       lobbyist_name: row.search(:td)[1].inner_text,
-       position: row.search(:td)[2].inner_text,
-       former_government_representative: row.search(:td)[3].inner_text,
-       cessation_date: row.search(:td)[4].inner_text.strip}
+    lobbyists_csv << agency_page.at('#lobbyistDetails').at(:tbody).search(:tr).map do |row|
+      [agency_id,
+       agency[:business_entity_name],
+       row.search(:td)[1].inner_text,
+       row.search(:td)[2].inner_text,
+       row.search(:td)[3].inner_text,
+       row.search(:td)[4].inner_text.strip]
     end
-
-    ScraperWiki.save_sqlite([:agency_id, :lobbyist_name], lobbyists, 'lobbyists')
   end
 
   # Save clients
   if agency_page.at('#clientDetails')
-    clients = agency_page.at('#clientDetails').at(:tbody).search(:tr).map do |row|
-      {agency_id: agency_id,
-       agency_business_entity_name: agency[:business_entity_name],
-       client_name: row.search(:td)[1].inner_text}
+    clients_csv << agency_page.at('#clientDetails').at(:tbody).search(:tr).map do |row|
+      [agency_id, agency[:business_entity_name], row.search(:td)[1].inner_text]
     end
-
-    ScraperWiki.save_sqlite([:agency_id, :client_name], clients, 'clients')
   end
 end
